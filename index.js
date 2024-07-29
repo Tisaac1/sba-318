e493d41f5ba955a3af116c955e616056a6bb13c3
Tisaac1 <lashay_isaac@yahoo.com>
2024-07-29 14:10:52 -0400

    upda3

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+const index = require('./User')
+ 
  //test
 const express = require('express');
 const hostname = express();
-const port = 3000;
+const port = 4000;
 
 
-
+const reqFilter = (reqObject,resObject, next) => {
+    console.log("reqFilter");
+     next(); 
  const server = express((req, res) => {
-    res.statusCode =200;
     res.setHeader ('Content-Type', 'text/html');
     res.write('<h1 style="color: pink" >This is my webpage!</h1>');
     res.write('<p>testing..testing..1.2.3..</p>');
     res.end();
 });
+}
+
+//Create and use error-handling middleware.
+hostname.use((err, req, res, next) => {
+    res.status(500).send(err.message);
+})
 
 hostname.listen(port, hostname, () => {
     console.log('Server listening at ${port}.');
@@ -22,15 +31,13 @@ const logReq = function (req, res, next) {
     next();
   };
 
-
-  hostname.get("/", (req, res) => {
-    res.send("testing testing 1 2 3");
+//Create and use at least two pieces of custom middleware.
+  hostname.get("/home", (req, res) => {
+    res.send("This is my server!");
   });
 
    hostname.use(logReq);
  console.log('Hello World')
 
 
-
 // console.log('Hello World')
-
